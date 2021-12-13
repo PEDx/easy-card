d0ca81e8d3a481ae68988e55826ae7c3073a7482
pengdong <pengdong5566@163.com>
2021-12-13 10:11:39 +0800

    feat: 添加构建产物

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
   },
 })
